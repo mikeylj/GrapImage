f4b4c8ec8812a9e60351b5974df6b6c1c5bb584b
lijun30 <lijun30@bj-m-207105a.local>
2017-07-08 21:59:53 +0800

    train

---
+++ train/train_images_1.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense, Input
+from keras import applications
+from keras.utils import to_categorical #多分类
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
+from keras.models import load_model
+import cv2
+import os
+from keras import backend as K
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+top_model_weights_path = './trained_vgg16/bottleneck_fc_model_weights.h5'
+train_data_dir = '/home/ylj/tag_sys/PIC_DATA/train'
+validation_data_dir = '/home/ylj/tag_sys/PIC_DATA/validation'
+dic_num = 8
+
+nb_train_samples = 1500 * dic_num
+nb_validation_samples = 500 * dic_num
+epochs = 50
+batch_size = 16
+
+#设定GPU使用内存大小(Tensorflow backend)
+def get_session(gpu_fraction=0.3):
+    '''Assume that you have 6GB of GPU memory and want to allocate ~2GB'''
+
+    num_threads = os.environ.get('OMP_NUM_THREADS')
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
+
+    if num_threads:
+        return tf.Session(config=tf.ConfigProto(
+            gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
+    else:
+        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+# KTF.set_session(get_session())
+
+
+def save_bottlebeck_features():
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
+
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_train = model.predict_generator(
+        generator, nb_train_samples // batch_size)
+    np.save(open('./trained_vgg16/bottleneck_features_train.npy', 'w'),
+            bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_validation = model.predict_generator(
+        generator, nb_validation_samples // batch_size)
+    np.save(open('./trained_vgg16/bottleneck_features_validation.npy', 'w'),
+            bottleneck_features_validation)
+
+
+def train_top_model():
+    train_data = np.load(open('./trained_vgg16/bottleneck_features_train.npy'))
+    # train_labels = np.array(
+    #     [0] * 3000 + [1] * 3000 + [2] * 3000 + [3] * 3000 + [4] * 2992)
+    labels = []
+    for i in range(dic_num):
+        for j in range(1500):
+            labels.append(i)
+    train_labels = np.array(
+        labels
+    )
+    # for k, v in enumerate(train_labels):
+    #     print k, '====>', v
+
+    validation_data = np.load(open('./trained_vgg16/bottleneck_features_validation.npy'))
+    # validation_labels = np.array(
+    #     [0] * 500 + [1] *500 + [2] * 500 + [3] * 500 + [4] * 496)
+    labels = []
+    for i in range(dic_num):
+        for j in range(500):
+            labels.append(i)
+    validation_labels = np.array(
+        labels
+    )
+    print validation_labels.shape
+    for k, v in enumerate(validation_labels):
+        print k, '====>', v
+
+    train_labels = to_categorical(np.array(train_labels))  # 转numpy数组
+    validation_labels = to_categorical(np.array(validation_labels))
+
+    model = Sequential()
+    model.add(Flatten(input_shape=train_data.shape[1:]))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(dic_num, activation='softmax'))
+
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+
+    print train_data.shape
+    print train_labels.shape
+
+    print validation_data.shape
+    print validation_labels.shape
+    model.fit(train_data, train_labels,
+              epochs=epochs,
+              batch_size=batch_size,
+              validation_data=(validation_data, validation_labels))
+    # model.save_weights(top_model_weights_path)
+    model.save_weights(top_model_weights_path)
+#训练
+save_bottlebeck_features()
+train_top_model()
+
+# model = Sequential()
+# model =load_model(top_model_weights_path)
+# model.compile(loss='binary_crossentropy',
+#               optimizer='rmsprop',
+#               metrics=['accuracy'])
+
+# img = cv2.imread('./data/train/梅花/125.jpg')
+# img = cv2.resize(img,(150,150))
+# img = np.reshape(img,[1,150,150,3])
+# classes = model.predict_classes(img)
+# print classes
+
+# #测试
+# datagen = ImageDataGenerator(rescale=1. / 255)
+#
+# input_tensor = Input(shape=(150,150,3))
+# # build the VGG16 network
+# # model = applications.VGG16(include_top=False, weights='imagenet')
+# model = applications.VGG16(weights='imagenet',include_top= False,input_tensor=input_tensor)
+#
+# test_data_dir   = 'data/train/'
+# generator = datagen.flow_from_directory(
+#         test_data_dir,
+#         target_size=(img_width, img_height),
+#         batch_size=1,
+#         class_mode=None,
+#         shuffle=False)
+# bottleneck_features_train = model.predict_generator(
+#         generator, 6000)
+# print bottleneck_features_train.shape
+#
+# # build a classifier model to put on top of the convolutional model
+# top_model = Sequential()
+# top_model.add(Flatten(input_shape=model.output_shape[1:]))
+# top_model.add(Dense(256, activation='relu'))
+# top_model.add(Dropout(0.5))
+# top_model.add(Dense(dic_num, activation='softmax'))
+#
+# # note that it is necessary to start with a fully-trained
+# # classifier, including the top classifier,
+# # in order to successfully do fine-tuning
+# top_model.load_weights(top_model_weights_path)
+
+
+
+# model.predict()
+
+# print bottleneck_features_train.shape
+# #
+# classes = top_model.predict(bottleneck_features_train)
+# print classes
+# i = 1
+# for c in classes:
+#     print  i, '====>', np.argmax(c)
+#     i = i +1
+
+
+
---
+++ train/train_images_2.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense, Input
+from keras.engine.training import Model
+import numpy as np
+from keras.utils import to_categorical #多分类
+
+# path to the model weights files.
+# weights_path = '../keras/examples/vgg16_weights.h5'
+top_model_weights_path = './trained_vgg16/bottleneck_fc_model_weights.h5'
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = '/home/ylj/tag_sys/PIC_DATA/train'
+validation_data_dir = '/home/ylj/tag_sys/PIC_DATA/validation'
+
+dic_num = 8
+
+nb_train_samples = 1500 * dic_num
+nb_validation_samples = 500 * dic_num
+
+# nb_train_samples = 15000
+# nb_validation_samples = 2500
+epochs = 50
+batch_size = 16
+
+# build the VGG16 network
+input_tensor = Input(shape=(150,150,3))
+# model = applications.VGG16(weights='imagenet', include_top=False)
+base_model = applications.VGG16(weights='imagenet',include_top= False,input_tensor=input_tensor)
+# print('Model loaded.')
+
+# build a classifier model to put on top of the convolutional model
+top_model = Sequential()
+top_model.add(Flatten(input_shape=base_model.output_shape[1:]))
+top_model.add(Dense(256, activation='relu'))
+top_model.add(Dropout(0.5))
+top_model.add(Dense(dic_num, activation='softmax'))
+
+# note that it is necessary to start with a fully-trained
+# classifier, including the top classifier,
+# in order to successfully do fine-tuning
+top_model.load_weights(top_model_weights_path)
+
+# add the model on top of the convolutional base
+# model.add(top_model)
+model = Model(inputs= base_model.input, outputs= top_model(base_model.output))
+
+# set the first 25 layers (up to the last conv block)
+# to non-trainable (weights will not be updated)
+# for layer in model.layers[:25]:
+#     layer.trainable = False
+
+for i,layer in enumerate(model.layers):
+    print i, layer.name
+
+import os
+os._exit(0)
+
+for layer in model.layers[:17]:
+    layer.trainable = False
+for layer in model.layers[17:]:
+    layer.trainable = True
+
+# compile the model with a SGD/momentum optimizer
+# and a very slow learning rate.
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+              metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size,
+)
+
+model.save('./trained_vgg16/vgg_final_vgg19.h5')
