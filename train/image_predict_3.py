2bdaa12df8ff7cd3bfc84ccad3d69ba8299ad1e1
lijun30 <lijun30@bj-m-207105a.local>
2017-07-16 20:35:41 +0800

    aaaa

--- train/image_predict_3.py
+++ train/image_predict_3.py
@@ -3,10 +3,13 @@
 import keras
 from keras.models import load_model
 from keras.models import Sequential
-import cv2
+# import cv2
 import numpy as np
 from keras.preprocessing import image
 import sys
+import codecs
+import os
+import shutil
 
 def get_top_predictions(preds, top=5):
     """Decodes the prediction of an ImageNet model.
@@ -32,64 +35,64 @@ def get_top_predictions(preds, top=5):
         # results.append(result)
     return top_indices
 if __name__ == '__main__':
-    image_paths = [
-        # 'test_img/train/桂花/3701.jpg',
-        # 'test_img/train/桂花/3702.jpg',
-        # 'test_img/train/桂花/3703.jpg',
-        # 'test_img/train/梅花/4879.jpg',
-        # 'test_img/train/梅花/4880.jpg',
-        # 'test_img/train/梅花/4881.jpg',
-        # 'test_img/train/牡丹/6.jpg',
-        # 'test_img/train/牡丹/12.jpg',
-        # 'test_img/train/牡丹/23.jpg',
-        # 'test_img/train/荷花/3367.jpg',
-        # 'test_img/train/荷花/3368.jpg',
-        # 'test_img/train/荷花/3369.jpg',
-        # 'test_img/train/菊花/7713.jpg',
-        # 'test_img/train/菊花/7714.jpg',
-        # 'test_img/train/菊花/7715.jpg',
-        './images/gh_1.jpg',
-        './images/gh_2.jpg',
-        './images/gh_3.jpg',
-        './images/gh_4.jpg',
-        './images/gh_5.jpg',
-        './images/hh_1.jpg',
-        './images/hh_2.jpg',
-        './images/hh_3.jpg',
-        './images/hh_4.jpg',
-        './images/hh_5.jpg',
-        './images/jh_1.jpg',
-        './images/jh_2.jpg',
-        './images/jh_3.jpg',
-        './images/jh_4.jpg',
-        './images/jh_5.jpg',
-        './images/knx_1.jpg',
-        './images/knx_2.jpg',
-        './images/knx_3.jpg',
-        './images/knx_4.jpg',
-        './images/knx_5.jpg',
-        './images/mh_1.jpg',
-        './images/mh_2.jpg',
-        './images/mh_3.jpg',
-        './images/mh_4.jpg',
-        './images/mh_5.jpg',
-        './images/mlh_1.jpg',
-        './images/mlh_2.jpg',
-        './images/mlh_3.jpg',
-        './images/mlh_4.jpg',
-        './images/mlh_5.jpg',
-        './images/mb_1.jpg',
-        './images/mb_2.jpg',
-        './images/mb_3.jpg',
-        './images/mb_4.jpg',
-        './images/mb_5.jpg',
-        './images/yjx_1.jpg',
-        './images/yjx_2.jpg',
-        './images/yjx_3.jpg',
-        './images/yjx_4.jpg',
-        './images/yjx_5.jpg',
-
-    ]
+    # image_paths = [
+    #     # 'test_img/train/桂花/3701.jpg',
+    #     # 'test_img/train/桂花/3702.jpg',
+    #     # 'test_img/train/桂花/3703.jpg',
+    #     # 'test_img/train/梅花/4879.jpg',
+    #     # 'test_img/train/梅花/4880.jpg',
+    #     # 'test_img/train/梅花/4881.jpg',
+    #     # 'test_img/train/牡丹/6.jpg',
+    #     # 'test_img/train/牡丹/12.jpg',
+    #     # 'test_img/train/牡丹/23.jpg',
+    #     # 'test_img/train/荷花/3367.jpg',
+    #     # 'test_img/train/荷花/3368.jpg',
+    #     # 'test_img/train/荷花/3369.jpg',
+    #     # 'test_img/train/菊花/7713.jpg',
+    #     # 'test_img/train/菊花/7714.jpg',
+    #     # 'test_img/train/菊花/7715.jpg',
+    #     './images/gh_1.jpg',
+    #     './images/gh_2.jpg',
+    #     './images/gh_3.jpg',
+    #     './images/gh_4.jpg',
+    #     './images/gh_5.jpg',
+    #     './images/hh_1.jpg',
+    #     './images/hh_2.jpg',
+    #     './images/hh_3.jpg',
+    #     './images/hh_4.jpg',
+    #     './images/hh_5.jpg',
+    #     './images/jh_1.jpg',
+    #     './images/jh_2.jpg',
+    #     './images/jh_3.jpg',
+    #     './images/jh_4.jpg',
+    #     './images/jh_5.jpg',
+    #     './images/knx_1.jpg',
+    #     './images/knx_2.jpg',
+    #     './images/knx_3.jpg',
+    #     './images/knx_4.jpg',
+    #     './images/knx_5.jpg',
+    #     './images/mh_1.jpg',
+    #     './images/mh_2.jpg',
+    #     './images/mh_3.jpg',
+    #     './images/mh_4.jpg',
+    #     './images/mh_5.jpg',
+    #     './images/mlh_1.jpg',
+    #     './images/mlh_2.jpg',
+    #     './images/mlh_3.jpg',
+    #     './images/mlh_4.jpg',
+    #     './images/mlh_5.jpg',
+    #     './images/mb_1.jpg',
+    #     './images/mb_2.jpg',
+    #     './images/mb_3.jpg',
+    #     './images/mb_4.jpg',
+    #     './images/mb_5.jpg',
+    #     './images/yjx_1.jpg',
+    #     './images/yjx_2.jpg',
+    #     './images/yjx_3.jpg',
+    #     './images/yjx_4.jpg',
+    #     './images/yjx_5.jpg',
+    #
+    # ]
     # 桂花
     # 荷花
     # 菊花
@@ -99,6 +102,19 @@ if __name__ == '__main__':
     # 牡丹
     # 郁金香
 
+    disPathName = '/home/ylj/tag_sys/datas/'
+    test_dir = os.path.join(disPathName, 'test')
+    image_paths = []
+
+    for dir_path, dir_names, file_names in os.walk(test_dir):
+        print dir_path
+        # print file_names
+        if  len(file_names) > 2:
+            for f in file_names:
+                print dir_path + '/' + f
+                image_paths.append(dir_path + '/' + f)
+    # print image_paths
+    # exit(0)
     model = load_model('trained_vgg16/vgg_final_vgg19.h5')
     # for arg in sys.argv:
     #     if "jpg" in arg:
