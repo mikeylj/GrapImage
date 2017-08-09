197faca0a4a6b67973fc2f94ca7762bc0f446862
lijun30 <lijun30@bj-m-207105a.local>
2017-08-09 12:10:21 +0800

    MulDownloadImages.py

--- DownloadOnePic.py
+++ DownloadOnePic.py
@@ -32,10 +32,10 @@ def download_link(directory, link):
     filename = m2.hexdigest();
     download_path = '%s/%s' % (directory, filename + ".jpg") #os.path.join(directory, uuid.uuid1().__str__() + ".jpg")
     if checkFileExsist(download_path):
-        logger.info('文件已存在 %s', link)
+        print ('文件已存在 %s', link)
         return True
 
-    logger.info('Downloading %s', link)
+    print('Downloading %s', link)
 
     try:
         headers = {
