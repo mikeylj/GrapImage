3b136bf792b179025f8d808b61e9c892c5d937bd
lijun30 <lijun30@bj-m-207105a.local>
2017-08-20 10:22:00 +0800

    MulDownloadImages.py

--- VGG_16_Predict.py
+++ VGG_16_Predict.py
@@ -10,6 +10,30 @@ model = ResNet50(weights='imagenet')
 
 pathName = '/home/ylj/tag_sys/GrapImage/baike_fl'
 
+def get_top_predictions(preds, top=5):
+    """Decodes the prediction of an ImageNet model.
+
+    # Arguments
+        preds: Numpy tensor encoding a batch of predictions.
+        top: integer, how many top-guesses to return.
+
+    # Returns
+        A list of lists of top class prediction tuples
+        `(class_name, class_description, score)`.
+        One list of tuples per sample in batch input.
+
+    # Raises
+        ValueError: in case of invalid shape of the `pred` array
+            (must be 2D).
+    """
+    results = []
+    for pred in preds:
+        top_indices = pred.argsort()[-top:][::-1]
+        # result = [tuple(CLASS_INDEX[str(i)]) + (pred[i],) for i in top_indices]
+        # result.sort(key=lambda x: x[2], reverse=True)
+        # results.append(result)
+    return top_indices
+
 def getClasses(path):
     image_paths = []
     for dir_path, dir_names, file_names in os.walk(path):
@@ -27,7 +51,20 @@ def getClasses(path):
     return image_paths;
 
 images = getClasses(pathName)
-print images
+
+for p in images:
+    img = image.load_img(p, target_size=(150, 150))
+    x = image.img_to_array(img).astype(np.float32) / 255
+    # print(x.shape)
+    x = np.expand_dims(x, axis=0)
+    # print(x.shape)
+    probs = model.predict(x)
+    # print p, np.argmax(probs)
+    print p, get_top_predictions(probs, 3)
+
+
+
+
 # img_path = '/home/ylj/tag_sys/PIC_DATA/train/桉叶藤/c_816669_0_3813.jpeg'
 # img = image.load_img(img_path, target_size=(224, 224))
 # x = image.img_to_array(img)
