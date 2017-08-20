cdc2ebc64a96c92356af2ebbe72c509428b703fd
lijun30 <lijun30@bj-m-207105a.local>
2017-08-20 10:18:49 +0800

    MulDownloadImages.py

--- VGG_16_Predict.py
+++ VGG_16_Predict.py
@@ -11,18 +11,23 @@ model = ResNet50(weights='imagenet')
 pathName = '/home/ylj/tag_sys/GrapImage/baike_fl'
 
 def getClasses(path):
+    image_paths = []
     for dir_path, dir_names, file_names in os.walk(path):
         if '.DS_Store' in file_names:
             file_names.remove('.DS_Store')
             p = os.path.join(dir_path, '.DS_Store')
             os.remove(p)
             print p
-        fileNums = len(file_names)
+        for f in file_names:
+            # print dir_path + '/' + f
+            image_paths.append(dir_path + '/' + f)
+        # fileNums = len(file_names)
+        # print dir_path, fileNums
 
-        print dir_path, fileNums
+    return image_paths;
 
-
-getClasses(pathName)
+images = getClasses(pathName)
+print images
 # img_path = '/home/ylj/tag_sys/PIC_DATA/train/桉叶藤/c_816669_0_3813.jpeg'
 # img = image.load_img(img_path, target_size=(224, 224))
 # x = image.img_to_array(img)
