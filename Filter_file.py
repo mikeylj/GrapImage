85f950a5030d57b42df9d500d11b7d2d7f05cda2
lijun30 <lijun30@bj-m-207105a.local>
2017-08-28 17:06:17 +0800

    OK

--- Filter_file.py
+++ Filter_file.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import hashlib
+from netOp import NETOP
 
 if __name__ == '__main__':
     sid = sys.argv[1]
@@ -12,9 +13,18 @@ if __name__ == '__main__':
     m2.update(url)
     filename = m2.hexdigest();
     filename    = download_path = '%s/%s' % (path, filename + ".jpg")
+    #文件不存在，则下载
     if not os.path.isfile(filename):
         print 'python DownloadOnePic.py "%s" "%s"' % (url, path)
         os.system('python DownloadOnePic.py "%s" "%s" ' % (url, path))
+    #文件存在，判断文件是否与名字相同
+    strName = NETOP.getImageNameFromBaidu(filename)
+    name = filename.split("/")[-2]
 
+    print strName, name
 
-    print sid, url, path, filename
+    # if not NETOP.checkName(name, str):
+    #     os.unlink(p)
+    #     print "已删除文件：", p
+    #
+    # print sid, url, path, filename
