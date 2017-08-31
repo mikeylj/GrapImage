3453b052287eae22c551bda2c770c1732de47d23
lijun30 <lijun30@bj-m-207105a.local>
2017-08-31 23:00:25 +0800

    OK

--- check_img_ext.py
+++ check_img_ext.py
@@ -17,7 +17,7 @@ for dirname in os.listdir(root_dir):
 		arr_pic_name_ext = os.path.splitext(pic_name)
 		if not arr_pic_name_ext[1] == "":
 			continue
-		pic_full_name = flow_name_dir+pic_name
+		pic_full_name = flow_name_dir+pic_name.replace("&", "\&")
 		pic_type = imghdr.what(pic_full_name)
 		os.rename(pic_full_name,pic_full_name+"."+pic_type)
 		print "pic_full_name:"+pic_full_name
