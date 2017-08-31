15dfb99abd7aff772b81316fd8a05f0eb6a79b85
lijun30 <lijun30@bj-m-207105a.local>
2017-08-31 23:37:44 +0800

    OK

---
+++ deal_baike/Deal_ImgFile.py
@@ -0,0 +1,46 @@
+#coding: utf-8
+import sys
+import os
+import hashlib
+from netOp import NETOP
+from mysql import Mysql
+
+if __name__ == '__main__':
+    sid = sys.argv[1]
+    url = sys.argv[2]
+    path = sys.argv[3]
+
+    m2 = hashlib.md5()
+    m2.update(url)
+    filename = m2.hexdigest();
+    filename    = download_path = '%s/%s' % (path, filename + ".jpg")
+    #文件不存在，则下载
+    if not os.path.isfile(filename):
+        print '下载文件：python DownloadOnePic.py "%s" "%s"' % (url, path)
+        os.system('python DownloadOnePic.py "%s" "%s" ' % (url, path))
+    #文件存在，判断文件是否与名字相同
+    op = NETOP()
+    print filename, sid, url, path
+    strName = op.getImageNameFromBaidu(filename)
+    name = filename.split("/")[-3]
+
+    # print strName, name, filename
+
+    if not op.checkName(name, strName):
+        os.unlink(filename)
+        print "已删除文件：", filename, name, strName
+        # sql = "update new_bing_flower set isdel=1 where id=%s" % sid
+        db = Mysql()
+        f_status_dict = {
+            'isdel' : 1,
+            'intro':strName
+        }
+        db.upDate('new_bing_flower', f_status_dict, " id='%s'" % sid)
+    else:
+        print "正常：", filename, name, strName
+        db = Mysql()
+        f_status_dict = {
+            'isdel': 2,
+            'intro': strName
+        }
+        db.upDate('new_bing_flower', f_status_dict, " id='%s'" % sid)
---
+++ deal_baike/Deal_ImgFiles.py
@@ -0,0 +1,63 @@
+#coding: utf-8
+
+import commands
+from mysql import Mysql
+from time import sleep, time
+import os
+
+#从数据库中取出总数
+def getTableTotal(tablename):
+    sql = 'select count(1) from %s where isdel=0'%tablename;
+    db = Mysql()
+    query = db.queryDataBySql(sql)
+    return query
+
+#从数据库中取出一页
+def getPage(tablename, page = 1, pagesize = 10):
+    if page < 1 :
+        page = 1
+    offset  = (page - 1) * pagesize
+    end     = offset + pagesize
+    sql = 'select * from %s where isdel = 0 limit %s, %s' % (tablename, offset, end)
+    db = Mysql()
+    query = db.queryDataBySql(sql)
+    return query
+
+#取得当前正在执行的命令数量
+def getCurrExecNum( command ):
+    output = 'ps aux|grep "python %s"|grep -v grep|wc -l' % command
+    (status, output) = commands.getstatusoutput(output)
+    return output
+def ExecMul(count, commands):
+    exec_command = 'Deal_ImgFile.py'
+    for row in commands:
+        sid = row[0]
+        url = row[1]
+        sclass= row[2].strip()
+        sub_class = row[3].strip()
+
+
+        path = 'new_bing_flower/%s/%s' % (sclass, sub_class)
+        s = 'python %s "%s" "%s" "%s" >> /tmp/ylj.log &' % (exec_command, sid, url, path)
+        print s
+        os.system(s)
+        # print getCurrExecNum(exec_command)
+        while (int(getCurrExecNum(exec_command)) > count):
+            print "Current Proc:" + getCurrExecNum(exec_command)
+            sleep(1)
+
+
+
+
+
+# newBaiDs    = getPage('new_bing_flower', 1, 10000)
+#
+# ExecMul(10, newBaiDs)
+root_dir = "/home/ylj/tag_sys/GrapImage/baike_fl/download/"
+for dirname in os.listdir(root_dir):
+    flow_name_dir = root_dir + dirname + "/"
+    for pic_name in os.listdir(flow_name_dir):
+        pic_full_name = flow_name_dir+pic_name
+        print pic_full_name
+
+
