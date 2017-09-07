3dd47063d3b327d92800b3fa3c37e4437e9dd0f6
lijun30 <lijun30@bj-m-207105a.local>
2017-09-07 16:11:56 +0800

    OK

---
+++ PreTrain_4.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+nums = 100
+import codecs
+import os
+import shutil
+from os.path import join, getsize
+import imghdr
+from PIL import Image
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+train_num   = 800
+test_num    = 160
+pathName = '/home/ylj/tag_sys/GrapImage/baike_fl/download_deal/'
+
+# train_dir = os.path.join(disPathName, 'train')
+# validation_dir = os.path.join(disPathName, 'validation')
+# test_dir = os.path.join(disPathName, 'test')
+# if not os.path.exists(train_dir):
+#     os.makedirs(train_dir)
+#
+# if not os.path.exists(validation_dir):
+#     os.makedirs(validation_dir)
+#
+# if not os.path.exists(test_dir):
+#     os.makedirs(test_dir)
+
+for dir_path, dir_names, file_names in os.walk(pathName):
+    fileNums = len(file_names)
+
+
+    if fileNums > (train_num + test_num):
+        print dir_path, fileNums
